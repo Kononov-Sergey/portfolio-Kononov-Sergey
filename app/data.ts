6b826b41b36f81b96d62904e29efa8095410c34c
Sergey Kononov <sergey.kononov@vk.team>
2024-05-22 20:56:35 +0300

    добавить ссылки

---
+++ app/data.ts
@@ -0,0 +1,330 @@
+const semesters1Data = [
+  {
+    name: "Английский",
+    link: "https://disk.yandex.ru/d/xBsbRTgPYeaf0Q",
+  },
+  {
+    name: "БЖД",
+    link: "https://disk.yandex.ru/d/z7oKRbuqiheStA",
+  },
+  {
+    name: "ИВТ",
+    link: "https://disk.yandex.ru/d/2_9aiNL1_Vcsxw",
+  },
+  {
+    name: "ИТ в математике",
+    link: "https://disk.yandex.ru/d/vW6scT7VwCxd1w",
+  },
+  {
+    name: "ИТ в физике",
+    link: "https://disk.yandex.ru/d/k9yx3IBmaMfpKw",
+  },
+  {
+    name: "Информатика ИВТ",
+    link: "https://disk.yandex.ru/d/5FkweTy20s9FOA",
+  },
+  {
+    name: "ЛАТМ",
+    link: "https://disk.yandex.ru/d/G5hoTDY623LsSQ",
+  },
+  {
+    name: "Основы компьютерной алгебры",
+    link: "https://disk.yandex.ru/d/I9OCkZRwxRSOdw",
+  },
+  {
+    name: "Физика",
+    link: "https://disk.yandex.ru/d/DlLRwY5aIreunQ",
+  },
+  {
+    name: "Экономика",
+    link: "https://disk.yandex.ru/d/7C-ZAeVCWOFF5w",
+  },
+  {
+    name: "Физкультура",
+    link: "https://disk.yandex.ru/d/8HX1H-mky1it8Q",
+  },
+];
+
+const semesters2Data = [
+  {
+    name: "Английский",
+    link: "https://disk.yandex.ru/d/bqv91KPG1cK7Zg",
+  },
+  {
+    name: "ИВТ",
+    link: "https://disk.yandex.ru/d/l_4gHeCduyW9_w",
+  },
+  {
+    name: "История",
+    link: "https://disk.yandex.ru/d/ST6FIRwIypQxaw",
+  },
+  {
+    name: "ЛАТМ",
+    link: "https://disk.yandex.ru/d/CsIe_4xBMT3nGQ",
+  },
+  {
+    name: "Практика",
+    link: "https://disk.yandex.ru/d/t-F46eo8VC2Y-w",
+  },
+  {
+    name: "Програмирование на языке C",
+    link: "https://disk.yandex.ru/d/LVeaUq61b3JFmg",
+  },
+  {
+    name: "Физика",
+    link: "https://disk.yandex.ru/d/lV9dW7IFMobP8g",
+  },
+  {
+    name: "Физкультура",
+    link: "https://disk.yandex.ru/d/qJ0T5S92R2WG0w",
+  },
+];
+
+const semesters3Data = [
+  {
+    name: "BigData",
+    link: "https://disk.yandex.ru/d/U8aetgrUx-9nPA",
+  },
+  {
+    name: "Web програмирование",
+    link: "https://disk.yandex.ru/d/-mOuCSVx54sSuw",
+  },
+  {
+    name: "Вычислительная техника",
+    link: "https://disk.yandex.ru/d/9IsPiipODodR6g",
+  },
+  {
+    name: "TKM",
+    link: "https://disk.yandex.ru/d/NAh3l_HNLxQ6uw",
+  },
+  {
+    name: "Практика",
+    link: "https://disk.yandex.ru/d/3sI2XJtsfqObPQ",
+  },
+  {
+    name: "Физика",
+    link: "https://disk.yandex.ru/d/C5IHxNiWjGZWYA",
+  },
+];
+
+const semesters4Data = [
+  {
+    name: "СМТ",
+    link: "https://disk.yandex.ru/d/jTO49xXY5vo82Q",
+  },
+  {
+    name: "DB",
+    link: "https://disk.yandex.ru/d/pc6aEwoOwQrd3g",
+  },
+  {
+    name: "Data",
+    link: "https://disk.yandex.ru/d/QtLcco2EvtwYFg",
+  },
+  {
+    name: "SWT",
+    link: "https://disk.yandex.ru/d/D2eRMVBGegxraA",
+  },
+  {
+    name: "Английский",
+    link: "https://disk.yandex.ru/d/P8PYaKgU6Zz0qw",
+  },
+  {
+    name: "Вычислительная техника",
+    link: "https://disk.yandex.ru/d/p-fzDK9TVuUqRA",
+  },
+  {
+    name: "Интегралы",
+    link: "https://disk.yandex.ru/d/TxsYWbm6I-hrqw",
+  },
+  {
+    name: "Курсовая",
+    link: "https://disk.yandex.ru/d/ooBxiYiDgf98tg",
+  },
+  {
+    name: "OC",
+    link: "https://disk.yandex.ru/d/ics4Pg9o_5m0Zg",
+  },
+  {
+    name: "Программирование на python",
+    link: "https://disk.yandex.ru/d/Qlv6FGxry7hfmQ",
+  },
+];
+
+const semesters5Data = [
+  {
+    name: "1C",
+    link: "https://disk.yandex.ru/d/1s8YrMYJfZ7mJQ",
+  },
+  {
+    name: "IT менеджмент",
+    link: "https://disk.yandex.ru/d/vzAnHOtsWsfHtQ",
+  },
+  {
+    name: "Английский",
+    link: "https://disk.yandex.ru/d/E_JzLSx2Q6EzjQ",
+  },
+  {
+    name: "ИТ в изучении иностранных языков",
+    link: "https://disk.yandex.ru/d/X2DADe1LvYlbmA",
+  },
+  {
+    name: "Издательские системы",
+    link: "https://disk.yandex.ru/d/0aW2zr_Z5dzyaw",
+  },
+  {
+    name: "Курсовая",
+    link: "https://disk.yandex.ru/d/RFSskb6bTISk_Q",
+  },
+  {
+    name: "Мат основы компьютерной графики",
+    link: "https://disk.yandex.ru/d/uuDU_CkwMB4m5w",
+  },
+  {
+    name: "Основы бизнес-информатики",
+    link: "https://disk.yandex.ru/d/VjwMVP3gU_Jq2g",
+  },
+  {
+    name: "Техника публикации информации",
+    link: "https://disk.yandex.ru/d/RVMZSiz1xu9HqQ",
+  },
+  {
+    name: "Практика",
+    link: "https://disk.yandex.ru/d/PNkuD0CqDXWdXw",
+  },
+];
+
+const semesters6Data = [
+  {
+    name: "3D графика",
+    link: "https://disk.yandex.ru/d/rKRgMeYo7tDSqg",
+  },
+  {
+    name: "Английский",
+    link: "https://disk.yandex.ru/d/hjdBVzN35woMdQ",
+  },
+  {
+    name: "Визуализация данных и инфографика",
+    link: "https://disk.yandex.ru/d/sZdNN8EsSouulQ",
+  },
+  {
+    name: "Докер",
+    link: "https://disk.yandex.ru/d/yx9TyCGn8xezGA",
+  },
+  {
+    name: "Защита информации",
+    link: "https://disk.yandex.ru/d/oIlLG0OFsbF8Ug",
+  },
+  {
+    name: "ИТ и С бизнес-аналитики",
+    link: "https://disk.yandex.ru/d/bbKzXRYTTHPPBQ",
+  },
+  {
+    name: "Основы корпоративного обучения",
+    link: "https://disk.yandex.ru/d/QamtuIzKjBmJew",
+  },
+  {
+    name: "Програмирование",
+    link: "https://disk.yandex.ru/d/83AsTdvr06hirg",
+  },
+  {
+    name: "Схемотехника",
+    link: "https://disk.yandex.ru/d/WcJcpALF7dDf6g",
+  },
+  {
+    name: "Электротехника",
+    link: "https://disk.yandex.ru/d/-e1if9DTrRO_dw",
+  },
+];
+
+const semesters7Data = [
+  {
+    name: "Английский",
+    link: "https://disk.yandex.ru/d/WWr9NFSd92hWrw",
+  },
+  {
+    name: "ИСП",
+    link: "https://disk.yandex.ru/d/IInR4GDbNhh2cg",
+  },
+  {
+    name: "Информационные средства и технологии инженерных и научных расчетов",
+    link: "https://disk.yandex.ru/d/78HMT-EpJUsBeA",
+  },
+  {
+    name: "Курсовая работа",
+    link: "https://disk.yandex.ru/d/6zLpFWViZoU_Bw",
+  },
+  {
+    name: "Практика",
+    link: "https://disk.yandex.ru/d/xzyMQP1w2O6xOw",
+  },
+];
+
+const semesters8Data = [
+  {
+    name: "ИТ рекрутмент",
+    link: "https://disk.yandex.ru/d/VxZ1hKWP8-momg",
+  },
+  {
+    name: "Английский",
+    link: "https://disk.yandex.ru/d/3BvwkQFc9_X2_Q",
+  },
+  {
+    name: "Информационные технологии оценки персонала",
+    link: "https://disk.yandex.ru/d/laotZCmXw-4sqA",
+  },
+  {
+    name: "Мировые информационные ресурсы и цифровые библиотеки",
+    link: "https://disk.yandex.ru/d/W6N5-x7lOHaBJQ",
+  },
+  {
+    name: "Научно-исследовательский проект",
+    link: "https://disk.yandex.ru/d/KCcwv882Kc5uaQ",
+  },
+  {
+    name: "Пакеты прикладных программ для статистической обработки и анализа данных",
+    link: "https://disk.yandex.ru/d/Wm38m5gI8o6lZw",
+  },
+  {
+    name: "Социальные и профессиональные вопросы информатики и ИТ",
+    link: "https://disk.yandex.ru/d/8iWOHGrvwUYc0Q",
+  },
+  {
+    name: "Языки написания спецификаций",
+    link: "https://disk.yandex.ru/d/dtXhrZLXsRiC6Q",
+  },
+];
+
+export const semestersData = [
+  {
+    name: "Семестр 1",
+    data: semesters1Data,
+  },
+  {
+    name: "Семестр 2",
+    data: semesters2Data,
+  },
+  {
+    name: "Семестр 3",
+    data: semesters3Data,
+  },
+  {
+    name: "Семестр 4",
+    data: semesters4Data,
+  },
+  {
+    name: "Семестр 5",
+    data: semesters5Data,
+  },
+  {
+    name: "Семестр 6",
+    data: semesters6Data,
+  },
+  {
+    name: "Семестр 7",
+    data: semesters7Data,
+  },
+  {
+    name: "Семестр 8",
+    data: semesters8Data,
+  },
+];
